8e6350473200be0ca979cd8a4e18830f6e342d3a
Hozifa Zeanalabdeen <pundit1d@gmail.com>
2024-04-06 07:27:34 -0500

    add solution architecture diagram

---
+++ lab3lab4/lab3lab4/src/test/java/com/lab3lab4/lab3lab4/Lab3lab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab3lab4.lab3lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3lab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
